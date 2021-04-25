1cc9dcc190c9098849e68634084ab6e04faeb473
Vik <jzenterprize90@gmail.com>
2021-04-25 15:19:45 -0400

    Learning how to call a main method in python

---
+++ TheMain.py
@@ -0,0 +1,33 @@
+def calculate_future_value(monthly_investment, yearly_interest, years):
+    #  converting yearly values to monthly ones
+    monthly_interest_rate = yearly_interest / 12 / 100
+    months = years * 12
+
+    #  calculate future value
+    future_value = 0.0
+    for i in range(0, months):
+        future_value += monthly_investment
+        monthly_interest = future_value * monthly_interest_rate
+        future_value += monthly_interest
+    return future_value
+
+
+def main():
+    choice = "y"
+    while choice.lower() == "y":
+        monthly_investment = float(input("Enter monthly investment:\t"))
+        yearly_interest_rate = float(input("Enter yearly interest rate:\t"))
+        years = int(input("Enter number of years:\t\t"))
+
+        future_value = calculate_future_value(monthly_investment, yearly_interest_rate, years)
+
+        print(f"Future value:\t\t\t {round(future_value, 2)}")
+        print()
+
+        choice = input("Continue? (y|n) ")
+        print()
+    print("Bye!")
+
+if __name__ == "__main__":
+    main()
+
